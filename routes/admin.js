d8d55891beb6f6a2c71f0cf0abd3f5081bdffb96
L200160149 <ums.informatika@gmail.com>
2022-02-07 15:48:52 +0700

    complete crud category

--- routes/admin.js
+++ routes/admin.js
@@ -2,9 +2,16 @@ const router = require('express').Router();
 const adminController = require('../controllers/adminController')
 
 router.get('/dashboard', adminController.viewDashboard);
+// endpoint category
 router.get('/category', adminController.viewCategory);
+router.post('/category', adminController.addCategory);
+router.put('/category', adminController.editCategory);
+router.delete('/category/:id', adminController.deleteCategory);
+// akhir endpoint category
 router.get('/bank', adminController.viewBank);
+
 router.get('/item', adminController.viewItem);
+
 router.get('/booking', adminController.viewBooking);
 
 module.exports = router;
