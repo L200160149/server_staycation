76f0876b235f5fe9f5ed18a07dbfe2bc6c79f3cd
L200160149 <ums.informatika@gmail.com>
2022-02-08 10:37:36 +0700

    compete crud and upload image bank

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,4 +1,7 @@
 const Category = require("../models/Category");
+const Bank = require("../models/Bank");
+const fs = require('fs-extra');
+const path = require('path');
 
 module.exports = {
   viewDashboard: (req, res) => {
@@ -71,12 +74,92 @@ module.exports = {
     }
   },
   // akhir route category
-
-  viewBank: (req, res) => {
-    res.render("admin/bank/view_bank", {
-      title: 'Staycation | Bank'
-    });
+  
+  // route bank
+  viewBank: async (req, res) => {
+    try {
+      const bank = await Bank.find();
+      // alert
+      const alertMessage = req.flash('alertMessage');
+      const alertStatus = req.flash('alertStatus');
+      const alert = { 
+        message: alertMessage, 
+        status: alertStatus,
+        title: 'Staycation | Bank'
+      };
+      res.render("admin/bank/view_bank", { 
+        bank,
+        alert,
+        title: 'Staycation | Bank'
+        });
+    } catch (error) {
+      res.redirect("/admin/bank");
+    }
   },
+  addBank: async (req, res) => {
+    try {
+      const { nameBank, nomorRekening, name } = req.body;
+      await Bank.create({ 
+        nameBank, 
+        nomorRekening, 
+        name,
+        imageUrl : `images/${req.file.filename}`
+      });
+      req.flash('alertMessage', 'Success Add Bank');
+      req.flash('alertStatus', 'success');
+      res.redirect("/admin/bank");
+    } catch (error) {
+      req.flash('alertMessage', `${error.message}`);
+      req.flash('alertStatus', 'danger');
+      res.redirect("/admin/bank");
+    }
+  },
+  editBank: async (req, res) => {
+    try {
+      const { id, nameBank, nomorRekening, name } = req.body;
+      const bank = await Bank.findOne({ _id: id });
+      if (req.file == undefined) {
+        bank.name = name;
+        bank.nameBank = nameBank;
+        bank.nomorRekening = nomorRekening;
+        await bank.save();
+        req.flash('alertMessage', 'Success Update Bank');
+        req.flash('alertStatus', 'success');
+        res.redirect("/admin/bank");
+      } else {
+        await fs.unlink(path.join(`public/${bank.imageUrl}`));
+        bank.name = name;
+        bank.nameBank = nameBank;
+        bank.nomorRekening = nomorRekening;
+        bank.imageUrl = `images/${req.file.filename}`;
+        await bank.save();
+        req.flash('alertMessage', 'Success Update Bank');
+        req.flash('alertStatus', 'success');
+        res.redirect("/admin/bank");
+      }
+    } catch (error) {
+      req.flash('alertMessage', `${error.message}`);
+      req.flash('alertStatus', 'danger');
+      res.redirect("/admin/bank");
+    }
+  },
+  deleteBank: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const bank = await Bank.findOne({ _id: id });
+      await fs.unlink(path.join(`public/${bank.imageUrl}`));
+      await bank.remove();
+      req.flash('alertMessage', 'Success Delete Bank');
+      req.flash('alertStatus', 'success');
+      res.redirect("/admin/bank");
+    } catch (error) {
+      req.flash('alertMessage', `${error.message}`);
+      req.flash('alertStatus', 'danger');
+      res.redirect("/admin/bank");
+    }
+  },
+  // akhir route bank
+
   viewItem: (req, res) => {
     res.render("admin/item/view_item", {
       title: 'Staycation | Item'
--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
-const adminController = require('../controllers/adminController')
+const adminController = require('../controllers/adminController');
+const { upload } = require('../middlewares/multer');
 
 router.get('/dashboard', adminController.viewDashboard);
 // endpoint category
@@ -8,7 +9,12 @@ router.post('/category', adminController.addCategory);
 router.put('/category', adminController.editCategory);
 router.delete('/category/:id', adminController.deleteCategory);
 // akhir endpoint category
+// endpoint bank
 router.get('/bank', adminController.viewBank);
+router.post('/bank', upload, adminController.addBank);
+router.put('/bank', upload, adminController.editBank);
+router.delete('/bank/:id', adminController.deleteBank);
+// akhir endpoint bank
 
 router.get('/item', adminController.viewItem);
 
