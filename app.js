72783329d46233f210c2786312bdca6518d78aae
L200160149 <ums.informatika@gmail.com>
2022-02-08 08:10:25 +0700

    complete flash validation message for category

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const methodOverride = require("method-override");
+// untuk notifikasi
 const session = require("express-session");
+const flash = require('connect-flash');
 // import mongoose
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://0.0.0.0:27017/bwa_mern");
@@ -20,7 +22,15 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(methodOverride("_method"));
-
+// untuk notifikasi
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 }
+}))
+app.use(flash());
+// akhir untuk notifikasi
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
