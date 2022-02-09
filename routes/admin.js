6ab4defdf98bb424bb1d0f6e2e6fc6810c7b8aa0
L200160149 <ums.informatika@gmail.com>
2022-02-09 09:47:31 +0700

    complete crud feature

--- routes/admin.js
+++ routes/admin.js
@@ -17,13 +17,18 @@ router.put('/bank', upload, adminController.editBank);
 router.delete('/bank/:id', adminController.deleteBank);
 // akhir endpoint bank
 
-// endpoint bank
+// endpoint item
 router.get('/item', adminController.viewItem);
 router.post('/item', uploadMultiple, adminController.addItem);
 router.get('/item/show-image/:id', adminController.showImageItem);
 router.get('/item/:id', adminController.showEditItem);
 router.put('/item/:id', uploadMultiple, adminController.editItem);
 router.delete('/item/:id/delete', adminController.deleteItem);
+
+router.get('/item/show-detail-item/:itemId', adminController.viewDetailItem);
+router.post('/item/add/feature', upload, adminController.addFeature);
+router.put('/item/update/feature', upload, adminController.editFeature);
+router.delete('/item/:itemId/feature/:id', adminController.deleteFeature);
 // akhir endpoint item
 
 router.get('/booking', adminController.viewBooking);
